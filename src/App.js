7ef5d8623489749ab56d167b599616ebb8c45937
Sandeepraj97 <sandeep2111997@gmail.com>
2024-11-23 12:36:00 +0530

    commit messge

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
-import React, { useState } from 'react';
-import MainScreen from './components/MainScreen';
-import MobileScreen from './components/MobileScreen';
-import Question from './components/Question';
-import './App.css';
 
-const App = () => {
-  const [players, setPlayers] = useState([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-
-  const questions = [
-    { question: 'What is 2 + 2?', options: ['A. 3', 'B. 4', 'C. 5', 'D. 6'], correct: "B. 4" },
-    { question: 'Capital of India?', options: ['A. Mumbai', 'B. Chennai', 'C. Delhi', 'D. Hydrabad'], correct: 'C. Delhi' },
-    { question: 'Which planet in the Milky Way is the hottest?', options: ['A. Venus', 'B. Earth', 'C. Pluto', 'D. Mars'], correct: 'A. Venus' },
-    { question: 'To which continent does India belong?', options: ['A. Asia', 'B. Aferica', 'C. Nortn America', 'D. Europe'], correct: 'A. Asia' },
-    { question: 'The Thar Desert in India is also known as what?', options: ['A. Asia', 'B. Aferica', 'C. Nortn America', 'D. Europe'], correct: 'A. Asia' }
-
-  ];
-
-  const currentQuestion = questions[currentQuestionIndex];
-
-  const handlePlayerJoin = (playerName) => {
-    setPlayers([...players, playerName]);
-  };
-
-  const handleAnswer = (selectedAnswer) => {
-    console.log(selectedAnswer)
-    if (selectedAnswer === currentQuestion.correct) {
-     alert('Correct!');
-
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-    } else {
-      alert('Wrong answer!');
+import questionData from "./questions.json"
+import "./App.css"
+import { useState } from "react"
+const  App = () =>{
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [score,setScore] = useState(0)
+    const [showScore,setShowScore] = useState(false)
+    const handleAnswerClick = (selectedOption) =>{
+        if(selectedOption === questionData[currentQuestion].correctOption){
+            setScore((prevScore) => prevScore + 1)
+        }
+        if (currentQuestion <questionData.length -1 ){
+            setCurrentQuestion((prevQuestion) => prevQuestion + 1)
+            
+        }else{
+            setShowScore(true)
+        }
     }
-  };
+    const handleReset = ()=>{
+        setCurrentQuestion(0)
+        setScore(0)
+        setShowScore(false)
+    }
+    return (
+        <>
+            <div className="qiz-app">
+            {
+                showScore ? (
+                    <div className="score-section" >
+                        <h2>You Score: {score}/{questionData.length}</h2>
+                        <button onClick={handleReset}>Restart</button>
+                    </div>
+                ): (
+                <div className="question-section">
+                    <h1>Question {currentQuestion+1}</h1>
+                    <p>{questionData[currentQuestion].question}</p>
+                    <div className="options">
+                        {
+                            questionData[currentQuestion].options.map((option,index)=>(
+                                <button onClick={()=> handleAnswerClick(option)} key={index}>{option}</button>
+                            ))
+                        }
+                    </div>
+                    
+                </div>
+                )
+            }
+            </div>
+        </>
+    )
+}
 
-  if (currentQuestionIndex >= questions.length) {
-    return <h1 className='game-over'>Game Over! Thanks for playing .</h1>;
-  }
-
-  return (
-    <div className='main-bg'>
-      <div className="app-container">
-        <MainScreen players={players} />
-        <MobileScreen onJoin={handlePlayerJoin} />
-        <Question question={currentQuestion} onAnswer={handleAnswer} />
-      </div>
-    </div>
-  );
-};
-
-export default App;
+export default App
